24152b7f30cc5cd584189dc02c9497ca11683e1b
Efrain Lemus <efraintlemusm@gmail.com>
2023-02-26 23:28:34 -0700

    work on timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,31 +9,64 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it's the 1st/3rd/5th/7th rep
+    count_down(work_sec)
+    # if it's the 8th rep:
+    count_down(long_break_sec)
+    # if it's the 2nd/4th/6th rep:
+    count_down(short_break_sec)
+    
+    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    # if count_sec == 0:
+    #     count_sec = '00'
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{'{:02d}'.format(count_sec)}")
+    print(count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
+
+
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
+
+
 
 #Label
 timer_title = Label(text='Timer',fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35))
 timer_title.grid(column=1, row=0)
 
 #Buttons
-btn_start = Button(text='Start', highlightthickness=0)
+btn_start = Button(text='Start', highlightthickness=0, command=start_timer)
 btn_start.grid(column=0, row=2)
 btn_reset = Button(text='Reset', highlightthickness=0)
 btn_reset.grid(column=2, row=2)
