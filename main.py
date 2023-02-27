9d3d25868c1e6032af9d0d2cef42a9e41baa6e25
Efrain Lemus <efraintlemusm@gmail.com>
2023-02-27 00:16:39 -0700

    finish pomodoro timer

--- main.py
+++ main.py
@@ -10,37 +10,72 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    global reps
+    reps = 0
+    timer_title.config(text='Timer', fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmark_label.config(text='')
+
+    # timer to zero
+    # title to timer
+    # reset checkmarks
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
+    reps += 1
+
+    print(f"Reps: {reps}")
+
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
     # If it's the 1st/3rd/5th/7th rep
-    count_down(work_sec)
-    # if it's the 8th rep:
-    count_down(long_break_sec)
-    # if it's the 2nd/4th/6th rep:
-    count_down(short_break_sec)
+    if reps % 2 != 0:
+        count_down(work_sec)
+        timer_title.config(text='Work', fg=GREEN)
+    # # if it's the 8th rep:
+    elif reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_title.config(text='Break', fg=RED)
+    # # if it's the 2nd/4th/6th rep:
+    elif reps % 2 == 0 and reps % 8 != 0:
+        count_down(short_break_sec)
+        timer_title.config(text='Break', fg=PINK)
+
+    # count_down(5)
     
-    count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
 
+    global reps
     count_min = math.floor(count / 60)
     count_sec = count % 60
     # if count_sec == 0:
     #     count_sec = '00'
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{'{:02d}'.format(count_sec)}")
-    print(count)
+    # print(count)
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    elif count == 0:
+        # reps += 1
+        # print(f"{reps}")
+        # print('After countdown')
+        start_timer()
+        mark = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            mark +='✓'
+        checkmark_label.config(text=mark)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,11 +103,11 @@ timer_title.grid(column=1, row=0)
 #Buttons
 btn_start = Button(text='Start', highlightthickness=0, command=start_timer)
 btn_start.grid(column=0, row=2)
-btn_reset = Button(text='Reset', highlightthickness=0)
+btn_reset = Button(text='Reset', highlightthickness=0, command=reset_timer)
 btn_reset.grid(column=2, row=2)
 
 #Checkmark
-checkmark_label = Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, 'bold'))
+checkmark_label = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, 'bold'))
 checkmark_label.grid(column=1, row=3)
 
 window.mainloop()
